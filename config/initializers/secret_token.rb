b7085edefc1ec36f564f0c74b6f7727159a99b89
Divya Kesapragada <divyakesapragada@gmail.com>
2015-03-25 00:15:17 +0530

    First commit for the shopping site project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoppingSite::Application.config.secret_key_base = 'ccaec83e5be524daeceb0d537c0d02e189ece27b48c7387df97640bedf86b6e06968c0f61d9ca3eaf7c4f2b7c8bf5d41fb35a0c836d3346392aeb06a7ca47e5d'
